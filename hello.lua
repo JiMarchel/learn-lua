6f240bb9dd2c447bcda80e443ccbe5eca6032b90
JiMarchel <jmarchel100@gmail.com>
2025-05-15 11:04:27 +0700

    var, global var, number, bolean, multi line string

---
+++ hello.lua
@@ -0,0 +1,20 @@
+-- local variable
+local name = "Mochammad Jimmy"
+print(name)
+
+-- global variable  
+Name = "Jimmy" .." " .. "Marchel"
+print(Name)
+
+-- number
+print(21 + 5)
+
+-- bool
+print(false)
+print(true)
+
+-- multi line string
+local abc = [[A
+B
+C]]
+print(abc)
